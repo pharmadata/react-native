a64ba10d64fd34d754b2a539d7a52d820d365b01
Rene Weber <weber.rene@live.de>
2017-06-20 09:24:54 -0700

    Add setNativeProps
    Summary:
    Curently FlatList does not implement setting native props directly like the old ListView did. This pr introduce the `setNativeProps` function which delegates to MetroListView or VirtualizedList. Thos don't have `setNativeProps` handling either, so, I delegated further to the respective ListView and Scroll components, which do have handling for it, thus, allowing to set the native props through FlatList.
    
    Create a project with a FlatList and change a native property using `setNativeProps`:
    
    ```javascript
      componentDidMount() {
            setInterval(() => {
                this.list.setNativeProps({ style: {backgroundColor:"white"} })
            }, 1000)
        }
    
      render() {
        return (
          <View style={styles.container}>
            <FlatList ref={component => this.list = component}
                      style={{backgroundColor:"black"}}
                      data={[{key: 'a'}, {key: 'b'}]}
                      renderItem={({item}) => <Text>{item.key}</Text>} />
          </View>
        )
      }
    ```
    
    Fixes #13501
    Closes https://github.com/facebook/react-native/pull/13529
    
    Differential Revision: D5283593
    
    Pulled By: sahrens
    
    fbshipit-source-id: 8f96f88e286042d82452fef924689b5a8a783987

--- Libraries/Lists/FlatList.js
+++ Libraries/Lists/FlatList.js
@@ -366,6 +366,12 @@ class FlatList<ItemT> extends React.PureComponent<DefaultProps, Props<ItemT>, vo
     }
   }
 
+  setNativeProps(props: Object) {
+    if (this._listRef) {
+      this._listRef.setNativeProps(props);
+    }
+  }
+
   componentWillMount() {
     this._checkProps(this.props);
   }
--- Libraries/Lists/VirtualizedList.js
+++ Libraries/Lists/VirtualizedList.js
@@ -283,6 +283,12 @@ class VirtualizedList extends React.PureComponent<OptionalProps, Props, State> {
     }
   }
 
+  setNativeProps(props: Object) {
+    if (this._scrollRef) {
+      this._scrollRef.setNativeProps(props);
+    }
+  }
+
   static defaultProps = {
     disableVirtualization: false,
     horizontal: false,
